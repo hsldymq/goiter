7d765a8fc4c518cac0332fc3be93166d0f89be8d
hsldymq <hsldymq@qq.com>
2025-11-28 12:57:21 +0800

    update iterator type def

--- iterator.go
+++ iterator.go
@@ -1,75 +1,75 @@
 package goiter
 
 import (
-    "iter"
+	"iter"
 )
 
 type SeqX[T any] interface {
-    ~func(yield func(T) bool)
+	~func(yield func(T) bool)
 }
 
-type Iterator[T any] iter.Seq[T]
+type Iterator[T any] func(yield func(T) bool)
 
 func (it Iterator[T]) Seq() iter.Seq[T] {
-    return iter.Seq[T](it)
+	return iter.Seq[T](it)
 }
 
 func (it Iterator[T]) WithCounter(startFrom int) Iterator2[int, T] {
-    return Zip(Counter(startFrom), it)
+	return Zip(Counter(startFrom), it)
 }
 
 func (it Iterator[T]) OrderBy(cmp func(T, T) int) Iterator[T] {
-    return OrderBy(it, cmp)
+	return OrderBy(it, cmp)
 }
 
 func (it Iterator[T]) StableOrderBy(cmp func(T, T) int) Iterator[T] {
-    return StableOrderBy(it, cmp)
+	return StableOrderBy(it, cmp)
 }
 
 func (it Iterator[T]) Filter(predicate func(T) bool) Iterator[T] {
-    return Filter(it, predicate)
+	return Filter(it, predicate)
 }
 
 func (it Iterator[T]) Take(n int) Iterator[T] {
-    return Take(it, n)
+	return Take(it, n)
 }
 
 func (it Iterator[T]) TakeLast(n int) Iterator[T] {
-    return TakeLast(it, n)
+	return TakeLast(it, n)
 }
 
 func (it Iterator[T]) Skip(n int) Iterator[T] {
-    return Skip(it, n)
+	return Skip(it, n)
 }
 
 func (it Iterator[T]) SkipLast(n int) Iterator[T] {
-    return SkipLast(it, n)
+	return SkipLast(it, n)
 }
 
 func (it Iterator[T]) Concat(its ...Iterator[T]) Iterator[T] {
-    return Concat(it, its...)
+	return Concat(it, its...)
 }
 
 func (it Iterator[T]) Reverse() Iterator[T] {
-    return Reverse(it)
+	return Reverse(it)
 }
 
 func (it Iterator[T]) Count() int {
-    return Count(it)
+	return Count(it)
 }
 
 func (it Iterator[T]) Through(f func(T) T) Iterator[T] {
-    return Transform(it, f)
+	return Transform(it, f)
 }
 
 func (it Iterator[T]) Cache() Iterator[T] {
-    return Cache(it)
+	return Cache(it)
 }
 
 func (it Iterator[T]) Once() Iterator[T] {
-    return Once(it)
+	return Once(it)
 }
 
 func (it Iterator[T]) FinishOnce() Iterator[T] {
-    return FinishOnce(it)
+	return FinishOnce(it)
 }
--- iterator2.go
+++ iterator2.go
@@ -1,81 +1,81 @@
 package goiter
 
 import (
-    "iter"
+	"iter"
 )
 
 type Seq2X[T1, T2 any] interface {
-    ~func(yield func(T1, T2) bool)
+	~func(yield func(T1, T2) bool)
 }
 
-type Iterator2[T1, T2 any] iter.Seq2[T1, T2]
+type Iterator2[T1, T2 any] func(yield func(T1, T2) bool)
 
 func (it Iterator2[T1, T2]) Seq() iter.Seq2[T1, T2] {
-    return iter.Seq2[T1, T2](it)
+	return iter.Seq2[T1, T2](it)
 }
 
 // PickV1 returns an iterator that yields the first value of each pair.
 func (it Iterator2[T1, T2]) PickV1() Iterator[T1] {
-    return PickV1(it)
+	return PickV1(it)
 }
 
 // PickV2 returns an iterator that yields the second value of each pair.
 func (it Iterator2[T1, T2]) PickV2() Iterator[T2] {
-    return PickV2(it)
+	return PickV2(it)
 }
 
 func (it Iterator2[T1, T2]) OrderBy(cmp func(*Combined[T1, T2], *Combined[T1, T2]) int) Iterator2[T1, T2] {
-    return Order2By(it, cmp)
+	return Order2By(it, cmp)
 }
 
 func (it Iterator2[T1, T2]) StableOrderBy(cmp func(*Combined[T1, T2], *Combined[T1, T2]) int) Iterator2[T1, T2] {
-    return StableOrder2By(it, cmp)
+	return StableOrder2By(it, cmp)
 }
 
 func (it Iterator2[T1, T2]) Filter(cmp func(T1, T2) bool) Iterator2[T1, T2] {
-    return Filter2(it, cmp)
+	return Filter2(it, cmp)
 }
 
 func (it Iterator2[T1, T2]) Take(n int) Iterator2[T1, T2] {
-    return Take2(it, n)
+	return Take2(it, n)
 }
 
 func (it Iterator2[T1, T2]) TakeLast(n int) Iterator2[T1, T2] {
-    return TakeLast2(it, n)
+	return TakeLast2(it, n)
 }
 
 func (it Iterator2[T1, T2]) Skip(n int) Iterator2[T1, T2] {
-    return Skip2(it, n)
+	return Skip2(it, n)
 }
 
 func (it Iterator2[T1, T2]) SkipLast(n int) Iterator2[T1, T2] {
-    return SkipLast2(it, n)
+	return SkipLast2(it, n)
 }
 
 func (it Iterator2[T1, T2]) Concat(its ...Iterator2[T1, T2]) Iterator2[T1, T2] {
-    return Concat2(it, its...)
+	return Concat2(it, its...)
 }
 
 func (it Iterator2[T1, T2]) Reverse() Iterator2[T1, T2] {
-    return Reverse2(it)
+	return Reverse2(it)
 }
 
 func (it Iterator2[T1, T2]) Count() int {
-    return Count2(it)
+	return Count2(it)
 }
 
 func (it Iterator2[T1, T2]) Through(f func(T1, T2) (T1, T2)) Iterator2[T1, T2] {
-    return Transform2(it, f)
+	return Transform2(it, f)
 }
 
 func (it Iterator2[T1, T2]) Cache() Iterator2[T1, T2] {
-    return Cache2(it)
+	return Cache2(it)
 }
 
 func (it Iterator2[T1, T2]) Once() Iterator2[T1, T2] {
-    return Once2(it)
+	return Once2(it)
 }
 
 func (it Iterator2[T1, T2]) FinishOnce() Iterator2[T1, T2] {
-    return FinishOnce2(it)
+	return FinishOnce2(it)
 }
